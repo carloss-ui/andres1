173c000a40a917b94314e3d305178fa7c1411093
eva002 <a90211778@gmail.com>
2025-07-29 17:13:18 -0400

    firts comint

---
+++ problema2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, d1, n2, d2;
+
+    while (true) {
+        cin >> n1 >> d1 >> n2 >> d2;
+
+        if (n1 == 0 && d1 == 0 && n2 == 0 && d2 == 0) {
+            break;
+        }
+
+        if (n1 * d2 == n2 * d1) {
+            cout << "=" << endl;
+        } else {
+            cout << "!=" << endl;
+        }
+    }
+
+    return 0;
+}
